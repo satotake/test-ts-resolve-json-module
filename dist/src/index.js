6e4dd0faa63e45bab07456f7a6161fe0503b3de6
SatowTakeshi <doublequotation@gmail.com>
2018-07-23 14:46:53 +0900

    cleanup tsconfig

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,10 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var package_json_1 = __importDefault(require("../package.json"));
-var world = package_json_1.default.name;
-function hello(word) {
-    if (word === void 0) { word = world; }
-    return "Hello " + world + "! ";
+const package_json_1 = __importDefault(require("../package.json"));
+const world = package_json_1.default.name;
+function hello(word = world) {
+    return `Hello ${world}! `;
 }
 exports.hello = hello;
